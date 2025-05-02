a6f6ee48483a1aac76a016c23a4c7e40147b65ee
Fernando Maciel <fernandonevesmaciel@gmail.com>
2025-05-02 19:17:14 -0300

    aula 6

---
+++ aula 6/index.js
@@ -0,0 +1,8 @@
+// Não podemos criar variáveis com palavras reservadas.
+// Variáveis precisam ter nomes significativos.
+// Não pode começa o nome de uma variável com um número
+// Não pode conter espaços ou traços
+// Ultilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize var, utilize let.
